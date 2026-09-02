ee67101592f9fb4d0d5a7c6bd032937d958470a5
SiteDevelop-pix <meltbrain@gmail.com>
2026-09-02 13:46:21 +0300

    Add pdfs and updated code

---
+++ subject-links.js
@@ -0,0 +1,25 @@
+window.subjectMeetConfig = {
+  "Англійська": "https://meet.google.com/phi-dznq-yrg?authuser=6",
+  "Англійська мова": "https://meet.google.com/phi-dznq-yrg?authuser=6",
+  "Алгебра": "https://meet.google.com/vni-cwzx-jxj?authuser=6&hs=179",
+  "Геометрія": "https://meet.google.com/phi-dznq-yrg?authuser=6",
+  "Фізична культура": "",
+  "Фінансова грамотність": "",
+  "Правознавство": "",
+
+  "Всесвітня історія": "https://meet.google.com/swu-zivk-oiw?authuser=6&hs=179",
+  "Історія Всесвітня": "https://meet.google.com/swu-zivk-oiw?authuser=6&hs=179",
+  "Історія України": "https://meet.google.com/rpy-cchw-uvg?authuser=6&hs=179",
+
+  "Географія": "https://meet.google.com/jud-yrfg-juh?authuser=6&hs=179",
+  "Українська мова": "https://meet.google.com/mwi-ejzd-ymz?authuser=6&hs=179",
+  "Українська література": "https://meet.google.com/yvh-qfer-rqt?authuser=6",
+  "Фізика": "https://meet.google.com/szn-nzfn-cwh?authuser=6&hs=179",
+  "Хімія": "https://meet.google.com/ruw-bgqp-rkw?authuser=6&hs=179",
+  "Біологія": "https://meet.google.com/mmr-jytx-onj?authuser=6&hs=179",
+  "Зарубіжна література": "https://meet.google.com/cpy-ursy-czc?authuser=6&hs=179",
+  "Німецька мова": "https://meet.google.com/zxm-wray-awh?authuser=6&hs=179",
+  "Інформатика": "",
+  "Захист вітчизни": "",
+  "Громадянська освіта": "https://meet.google.com/vci-gtcn-nvn?authuser=6&hs=179"
+};
