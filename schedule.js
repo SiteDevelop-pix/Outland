0c8ad993c458361d6c19eaa1c0d774b3602e75c4
SiteDevelop-pix <meltbrain@gmail.com>
2026-09-03 16:56:46 +0300

    Update project files

---
+++ schedule.js
@@ -0,0 +1,48 @@
+window.sputnikSchedule = {
+  'Понеділок': [
+    { number: 1, time: '08:00', subject: 'Англійська мова' },
+    { number: 2, time: '08:40', subject: 'Алгебра' },
+    { number: 3, time: '09:20', subject: 'Українська мова' },
+    { number: 4, time: '10:00', subject: 'Українська література' },
+    { number: 5, time: '10:40', subject: 'Всесвітня історія' },
+    { number: 6, time: '11:20', subject: 'Інформатика' },
+    { number: 7, time: '12:20', subject: 'Географія' }
+  ],
+  'Вівторок': [
+    { number: 1, time: '08:00', subject: 'Геометрія' },
+    { number: 2, time: '08:40', subject: 'Українська мова' },
+    { number: 3, time: '09:20', subject: 'Хімія' },
+    { number: 4, time: '10:00', subject: 'Зарубіжна література' },
+    { number: 5, time: '10:40', subject: 'Фізика' },
+    { number: 6, time: '11:20', subject: 'Англійська мова' },
+    { number: 7, time: '12:20', subject: 'Біологія' },
+    { number: 8, time: '13:00', subject: 'Фінансова грамотність' }
+  ],
+  'Середа': [
+    { number: 1, time: '08:00', subject: 'Англійська мова' },
+    { number: 2, time: '08:40', subject: 'Алгебра' },
+    { number: 3, time: '09:20', subject: 'Українська мова' },
+    { number: 4, time: '10:00', subject: 'Українська література' },
+    { number: 5, time: '10:40', subject: 'Всесвітня історія' },
+    { number: 6, time: '11:20', subject: 'Інформатика' },
+    { number: 7, time: '12:20', subject: 'Географія' }
+  ],
+  'Четвер': [
+    { number: 1, time: '08:00', subject: 'Українська мова' },
+    { number: 2, time: '08:40', subject: 'Громадянська освіта' },
+    { number: 3, time: '09:20', subject: 'Геометрія' },
+    { number: 4, time: '10:00', subject: 'Українська література' },
+    { number: 5, time: '10:40', subject: 'Фізика' },
+    { number: 6, time: '11:20', subject: 'Німецька мова' },
+    { number: 7, time: '12:20', subject: 'Фізична культура' }
+  ],
+  "П'ятниця": [
+    { number: 1, time: '08:00', subject: 'Фізика' },
+    { number: 2, time: '08:40', subject: 'Українська мова' },
+    { number: 3, time: '09:20', subject: 'Алгебра' },
+    { number: 4, time: '10:00', subject: 'Біологія' },
+    { number: 5, time: '10:40', subject: 'Хімія' },
+    { number: 6, time: '11:20', subject: 'Інформатика' },
+    { number: 7, time: '12:20', subject: "Здоров'я, безпека та добробут" }
+  ]
+};
