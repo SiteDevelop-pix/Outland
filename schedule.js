32e25827f36e3bca79888662f4a0218530d0a132
SiteDevelop-pix <meltbrain@gmail.com>
2026-09-04 13:35:55 +0300

    Update project files

--- schedule.js
+++ schedule.js
@@ -1,22 +1,22 @@
 window.sputnikSchedule = {
   'Понеділок': [
-    { number: 1, time: '08:00', subject: 'Англійська мова' },
-    { number: 2, time: '08:40', subject: 'Алгебра' },
+    { number: 1, time: '08:00', subject: 'Технології' },
+    { number: 2, time: '08:40', subject: 'Історія України' },
     { number: 3, time: '09:20', subject: 'Українська мова' },
-    { number: 4, time: '10:00', subject: 'Українська література' },
-    { number: 5, time: '10:40', subject: 'Всесвітня історія' },
-    { number: 6, time: '11:20', subject: 'Інформатика' },
-    { number: 7, time: '12:20', subject: 'Географія' }
+    { number: 4, time: '10:00', subject: 'Алгебра' },
+    { number: 5, time: '10:40', subject: 'Англійська мова' },
+    { number: 6, time: '11:20', subject: 'Обр. мистецтво' },
+    { number: 7, time: '12:20', subject: 'Біологія' }
   ],
   'Вівторок': [
-    { number: 1, time: '08:00', subject: 'Геометрія' },
-    { number: 2, time: '08:40', subject: 'Українська мова' },
-    { number: 3, time: '09:20', subject: 'Хімія' },
-    { number: 4, time: '10:00', subject: 'Зарубіжна література' },
-    { number: 5, time: '10:40', subject: 'Фізика' },
-    { number: 6, time: '11:20', subject: 'Англійська мова' },
-    { number: 7, time: '12:20', subject: 'Біологія' },
-    { number: 8, time: '13:00', subject: 'Фінансова грамотність' }
+    { number: 1, time: '08:00', subject: 'Хімія' },
+    { number: 2, time: '08:40', subject: 'Зарубіжна література' },
+    { number: 3, time: '09:20', subject: 'Біологія' },
+    { number: 4, time: '10:00', subject: 'Англійська мова' },
+    { number: 5, time: '10:40', subject: 'Німецька мова' },
+    { number: 6, time: '11:20', subject: 'Фізика' },
+    { number: 7, time: '12:20', subject: 'Геометрія' },
+    { number: 8, time: '13:00', subject: 'Фізична культура' }
   ],
   'Середа': [
     { number: 1, time: '08:00', subject: 'Англійська мова' },
@@ -42,7 +42,6 @@ window.sputnikSchedule = {
     { number: 3, time: '09:20', subject: 'Алгебра' },
     { number: 4, time: '10:00', subject: 'Біологія' },
     { number: 5, time: '10:40', subject: 'Хімія' },
-    { number: 6, time: '11:20', subject: 'Інформатика' },
-    { number: 7, time: '12:20', subject: "Здоров'я, безпека та добробут" }
+    { number: 6, time: '11:20', subject: 'Інформатика' }
   ]
 };
