43c4a8d155c3b246b8b645adda653c12f7749b13
SiteDevelop-pix <meltbrain@gmail.com>
2026-09-05 16:42:25 +0300

    Update project files

--- subject-links.js
+++ subject-links.js
@@ -1,16 +1,12 @@
 window.subjectMeetConfig = {
-  "Англійська": "https://meet.google.com/phi-dznq-yrg?authuser=6",
   "Англійська мова": "https://meet.google.com/phi-dznq-yrg?authuser=6",
   "Алгебра": "https://meet.google.com/vni-cwzx-jxj?authuser=6&hs=179",
   "Геометрія": "https://meet.google.com/zws-doix-hki?authuser=6&hs=179",
   "Фізична культура": "",
-  "Фінансова грамотність": "",
-  "Правознавство": "",
-
+  "Фінансова грамотність": "https://meet.google.com/yks-vzmh-wkz?authuser=6&hs=179",
+  "Правознавство": "https://meet.google.com/oqn-saxc-qyd?authuser=6&hs=179",
   "Всесвітня історія": "https://meet.google.com/swu-zivk-oiw?authuser=6&hs=179",
-  "Історія Всесвітня": "https://meet.google.com/swu-zivk-oiw?authuser=6&hs=179",
   "Історія України": "https://meet.google.com/rpy-cchw-uvg?authuser=6&hs=179",
-
   "Географія": "https://meet.google.com/jud-yrfg-juh?authuser=6&hs=179",
   "Українська мова": "https://meet.google.com/mwi-ejzd-ymz?authuser=6&hs=179",
   "Українська література": "https://meet.google.com/mwi-ejzd-ymz?authuser=6&hs=179",
@@ -19,7 +15,12 @@ window.subjectMeetConfig = {
   "Біологія": "https://meet.google.com/mmr-jytx-onj?authuser=6&hs=179",
   "Зарубіжна література": "https://meet.google.com/cpy-ursy-czc?authuser=6&hs=179",
   "Німецька мова": "https://meet.google.com/zxm-wray-awh?authuser=6&hs=179",
-  "Інформатика": "",
-  "Захист вітчизни": "",
-  "Громадянська освіта": "https://meet.google.com/vci-gtcn-nvn?authuser=6&hs=179"
+  "Громадянська освіта": "https://meet.google.com/vci-gtcn-nvn?authuser=6&hs=179",
+  "Обр. мистецтво": "https://meet.google.com/eab-dgth-vkw?authuser=6&hs=179",
+  "Здоров'я, безпека та добробут": "https://meet.google.com/nkc-bvte-ukr?authuser=6&hs=179"
+};
+
+window.informaticsGroupConfig = {
+  "1 група": "https://meet.google.com/vze-fugv-wkr?authuser=6&hs=179",
+  "2 група": "https://meet.google.com/uyq-czck-bot"
 };
